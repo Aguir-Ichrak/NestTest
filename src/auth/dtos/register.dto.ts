02800f2167a3e564d54e69fd65ce9398e8de93db
Ichrak Aguir <ichrakaguir123@gmail.com>
2025-02-21 21:06:33 +0100

    authentification_dto

---
+++ src/auth/dtos/register.dto.ts
@@ -0,0 +1,27 @@
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+} from 'class-validator';
+import { Role } from 'src/global.const';
+
+export class RegisterDto {
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @IsNotEmpty()
+  @IsEnum(Role)
+  role: Role;
+}
