021fb081a8aad0f2430102cb9f80615aa6466b87
Ichrak Aguir <ichrakaguir123@gmail.com>
2025-02-23 19:16:00 +0100

    replace_process.env_with_configService

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -3,11 +3,16 @@ import {
   ExecutionContext,
   UnauthorizedException,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AuthGuard as PassportAuthGuard } from '@nestjs/passport';
 import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class AuthGuard extends PassportAuthGuard('jwt') {
+  constructor(private configService: ConfigService) {
+    super();
+  }
+
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = await context.switchToHttp().getRequest();
     const authHeader = request.headers.authorization;
@@ -17,7 +22,7 @@ export class AuthGuard extends PassportAuthGuard('jwt') {
     }
 
     const token = authHeader.split(' ')[1];
-    const secret = process.env.JWT_SECRET;
+    const secret = this.configService.get<string>('JWT_SECRET');
 
     if (!secret) {
       throw new UnauthorizedException('Access denied: Missing JWT secret');
